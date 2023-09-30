62571e5e2b365c73e593c74efdff923c79ab6233
Romeo Mungiu <mungiu.romeo@gmail.com>
2023-09-30 22:52:03 +0200

    final

---
+++ PingProject/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace PingProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, CyberSA!");
+        }
+    }
+}
